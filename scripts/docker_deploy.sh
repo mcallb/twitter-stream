afbccb986d3e0604c1dc2897fdb2e2f7783b60f4
Brian McAllister <bmcallister@ebsco.com>
2017-04-22 19:45:59 -0400

    key change

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -9,7 +9,7 @@ docker rmi mcallb/twitter-stream:current
 docker tag mcallb/twitter-stream:latest mcallb/twitter-stream:current
 docker run -d \
     --restart always \
-    --env "AWS_ACCESS_KEY_ID=AKIAJUZEOPB5NNQHJORA" \
+    --env "AWS_ACCESS_KEY_ID=AKIAJBF4BX7R7H2EH4SA" \
     --env "AWS_SECRET_ACCESS_KEY=$aws_access_key" \
     --env "AWS_DEFAULT_REGION=us-east-1" \
     --name twitter-stream mcallb/twitter-stream:latest
