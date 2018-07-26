83b7e4af9202bc31b32aa169b14c21f7d03ecd59
Brian McAllister <bmcallister@ebsco.com>
2018-07-25 20:57:50 -0400

    More beers

--- stream.py
+++ stream.py
@@ -171,7 +171,8 @@ if __name__ == '__main__':
     
     SEARCH_FILTER = ['fort hill brewery','@FortHillBeer','@lamplighterbrew','@finbackbrewery','trilliumbrewing',
                      'trillium','maine beer company','maine beer co','foleybrothers','foley brothers','sazerac',
-                     'sip of sunshine','lawsonsfinest','lawsons','rhinegeist','captain lawrence','fiddlehead','beer\'d']
+                     'sip of sunshine','lawsonsfinest','lawsons','rhinegeist','captain lawrence','fiddlehead',
+                     'foreign objects','beer\'d']
     # FOLLOW_FILTER = get_follow_filer()
     # @RedstoneLiquors: 109292604
     # @rapidliquors: 198174347
