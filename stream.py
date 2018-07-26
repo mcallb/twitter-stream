6d9eae8d35a1bf9f1ef5b228ddb2eb322dea4776
Brian McAllister <bmcallister@ebsco.com>
2018-07-25 20:53:48 -0400

    Add new beer... I need to move these to a file.

--- stream.py
+++ stream.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
     
     SEARCH_FILTER = ['fort hill brewery','@FortHillBeer','@lamplighterbrew','@finbackbrewery','trilliumbrewing',
                      'trillium','maine beer company','maine beer co','foleybrothers','foley brothers','sazerac',
-                     'sip of sunshine','lawsonsfinest','lawsons','rhinegeist','beer\'d']
+                     'sip of sunshine','lawsonsfinest','lawsons','rhinegeist','captain lawrence','fiddlehead','beer\'d']
     # FOLLOW_FILTER = get_follow_filer()
     # @RedstoneLiquors: 109292604
     # @rapidliquors: 198174347
